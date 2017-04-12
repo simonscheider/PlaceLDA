b626e9bc3f45d6adabe991c22d6c12ad0aa3e768
simonscheider <simonscheider@web.de>
2017-04-12 20:36:30 +0200

    combine all sources

--- place_lda.py
+++ place_lda.py
@@ -12,88 +12,203 @@
 from __future__ import division, print_function
 
 import numpy as np
+import overpy
+import string
 import lda
 import lda.datasets
-
-# document-term matrix
-X = lda.datasets.load_reuters()
-##print("type(X): {}".format(type(X)))
-##print("shape: {}\n".format(X.shape))
-for i in range(10):
-    print(format(X[i]))
-
-
-# the vocab
-vocab = lda.datasets.load_reuters_vocab()
-##print("type(vocab): {}".format(type(vocab)))
-##print("len(vocab): {}\n".format(len(vocab)))
-
-# titles for each story
-titles = lda.datasets.load_reuters_titles()
-##print("type(titles): {}".format(type(titles)))
-##print("len(titles): {}\n".format(len(titles)))
-
-doc_id = 0
-word_id = 3117
-
-print("doc id: {} word id: {}".format(doc_id, word_id))
-print("-- count: {}".format(X[doc_id, word_id]))
-print("-- word : {}".format(vocab[word_id]))
-print("-- doc  : {}".format(titles[doc_id]))
-
-#Fit the model
-##model = lda.LDA(n_topics=20, n_iter=500, random_state=1)
-##model.fit(X)
-
-# topic-word probabilities
-##topic_word = model.topic_word_
-###print("type(topic_word): {}".format(type(topic_word)))
-##print("shape: {}".format(topic_word.shape))
-
-#check the first 5 normalized sums of probabilities
-##for n in range(5):
-##    sum_pr = sum(topic_word[n,:])
-##    print("topic: {} sum: {}".format(n, sum_pr))
-
-# get the top 5 words for each topic (by probablity)
-n = 5
-##for i, topic_dist in enumerate(topic_word):
-##    topic_words = np.array(vocab)[np.argsort(topic_dist)][:-(n+1):-1]
-##    print('*Topic {}\n- {}'.format(i, ' '.join(topic_words)))
-
-#Document topic probabilities
-##doc_topic = model.doc_topic_
-##print("type(doc_topic): {}".format(type(doc_topic)))
-##print("shape: {}".format(doc_topic.shape))
+import sys
+import os
+#sys.path.append(os.getcwd())
+import placewebscraper
+from googleplaces import GooglePlaces, types, lang
+#from geopy.geocoders import Nominatim
 
 
-# Get the most probable topic for each document:
-##for n in range(10):
-##    topic_most_pr = doc_topic[n].argmax()
-##    print("doc: {} topic: {}\n{}...".format(n,
-##                                            topic_most_pr,
-##                                            titles[n][:50]))
 
-#Most important: apply a given model to a new test dataset
+YOUR_API_KEY = 'AIzaSyA2O6G7eCxOFTbu1HjPuqpuLEnllSDQDB8'
 
-#X = lda.datasets.load_reuters()
-#titles = lda.datasets.load_reuters_titles()
-X_train = X[10:]
-X_test = X[:10]
-titles_test = titles[:10]
-model = lda.LDA(n_topics=20, n_iter=1500, random_state=1)
-model.fit(X_train)
-topic_word = model.topic_word_
-#print("type(topic_word): {}".format(type(topic_word)))
-print("shape: {}".format(topic_word.shape))
+def matchtoGoogleP(placename,lat, lng):
+    lat_lng = {}
+    lat_lng['lat']=lat
+    lat_lng['lng']=lng
+    google_places = GooglePlaces(YOUR_API_KEY)
+    query_result = google_places.text_search(placename,lat_lng,radius=300)
+    #if query_result.has_attributions:
+    #    print query_result.html_attributions
 
-# get the top 5 words for each topic (by probablity)
-n = 5
-for i, topic_dist in enumerate(topic_word):
-    topic_words = np.array(vocab)[np.argsort(topic_dist)][:-(n+1):-1]
-    print('*Topic {}\n- {}'.format(i, ' '.join(topic_words)))
+    place = query_result.places[0]
+    ##    print place.name
+    ##    print place.geo_location
+    ##    print place.place_id
 
-# apply topic model to new test data set
-doc_topic_test = model.transform(X_test)
-for title, topics in zip(titles_test, doc_topic_test):
-    print("{} (top topic: {})".format(title, topics.argmax()))
+    # The following method has to make a further API call.
+    place.get_details()
+    # Referencing any of the attributes below, prior to making a call to
+    # get_details() will raise a googleplaces.GooglePlacesAttributeError.
+    ##    print place.details # A dict matching the JSON response from Google.
+##    print place.website
+##    print place.types
+##    print place.details['opening_hours']
+##    #print place.details['reviews']
+##    if 'reviews' in place.details.keys():
+##        for r in place.details['reviews']:
+##            print r['text']
+##    print place.rating
+    return place
+
+def getCentroid(nodes):
+    points = [(n.lat,n.lon) for n in nodes]
+    x_coords = [p[0] for p in points]
+    y_coords = [p[1] for p in points]
+    _len = len(points)
+    centroid_x = sum(x_coords)/_len
+    centroid_y = sum(y_coords)/_len
+    return (centroid_x, centroid_y)
+
+def getOSMInfo(osmid, elementtype='node'):
+    keysofinterest = ['shop', 'amenity', 'leisure', 'tourism', 'historic', 'man_made']
+    api = overpy.Overpass()
+    #geolocator = Nominatim()
+
+    # We can also see a node's metadata:
+    osm = {}
+    result = api.query((elementtype+"({}); out;").format(osmid))
+    if elementtype == 'node':
+        res = result.get_node(osmid)
+        osm['lat'] = res.lat
+        osm['lon'] = res.lon
+    elif elementtype == 'way':
+        res = result.get_way(osmid)
+        c = getCentroid(res.get_nodes(resolve_missing=True))
+        osm['lat']=c[0]
+        osm['lon']=c[0]
+
+    #print(res.attributes)
+    osm['name'] = res.tags['name']
+    osm['keys'] = []
+    #location = geolocator.reverse(osmid)
+    #print(location.address)
+    for k in keysofinterest:
+        if k in res.tags.keys():
+            osm['keys'].append(k +':'+res.tags[k])
+    if 'website' in res.tags.keys():
+            osm['website'] = res.tags[k]
+    if 'opening_hours' in res.tags.keys():
+            osm['opening_hours'] = res.tags[k]
+    #print(osm)
+    return osm
+
+
+
+def extractLDATopics():
+    # document-term matrix
+    X = lda.datasets.load_reuters()
+    ##print("type(X): {}".format(type(X)))
+    ##print("shape: {}\n".format(X.shape))
+    for i in range(10):
+        print(format(X[i]))
+
+
+    # the vocab
+    vocab = lda.datasets.load_reuters_vocab()
+    ##print("type(vocab): {}".format(type(vocab)))
+    ##print("len(vocab): {}\n".format(len(vocab)))
+
+    # titles for each story
+    titles = lda.datasets.load_reuters_titles()
+    ##print("type(titles): {}".format(type(titles)))
+    ##print("len(titles): {}\n".format(len(titles)))
+
+    doc_id = 0
+    word_id = 3117
+
+    print("doc id: {} word id: {}".format(doc_id, word_id))
+    print("-- count: {}".format(X[doc_id, word_id]))
+    print("-- word : {}".format(vocab[word_id]))
+    print("-- doc  : {}".format(titles[doc_id]))
+
+    #Fit the model
+    ##model = lda.LDA(n_topics=20, n_iter=500, random_state=1)
+    ##model.fit(X)
+
+    # topic-word probabilities
+    ##topic_word = model.topic_word_
+    ###print("type(topic_word): {}".format(type(topic_word)))
+    ##print("shape: {}".format(topic_word.shape))
+
+    #check the first 5 normalized sums of probabilities
+    ##for n in range(5):
+    ##    sum_pr = sum(topic_word[n,:])
+    ##    print("topic: {} sum: {}".format(n, sum_pr))
+
+    # get the top 5 words for each topic (by probablity)
+    n = 5
+    ##for i, topic_dist in enumerate(topic_word):
+    ##    topic_words = np.array(vocab)[np.argsort(topic_dist)][:-(n+1):-1]
+    ##    print('*Topic {}\n- {}'.format(i, ' '.join(topic_words)))
+
+    #Document topic probabilities
+    ##doc_topic = model.doc_topic_
+    ##print("type(doc_topic): {}".format(type(doc_topic)))
+    ##print("shape: {}".format(doc_topic.shape))
+
+
+    # Get the most probable topic for each document:
+    ##for n in range(10):
+    ##    topic_most_pr = doc_topic[n].argmax()
+    ##    print("doc: {} topic: {}\n{}...".format(n,
+    ##                                            topic_most_pr,
+    ##                                            titles[n][:50]))
+
+    #Most important: apply a given model to a new test dataset
+
+    #X = lda.datasets.load_reuters()
+    #titles = lda.datasets.load_reuters_titles()
+    X_train = X[10:]
+    X_test = X[:10]
+    titles_test = titles[:10]
+    model = lda.LDA(n_topics=20, n_iter=1500, random_state=1)
+    model.fit(X_train)
+    topic_word = model.topic_word_
+    #print("type(topic_word): {}".format(type(topic_word)))
+    print("shape: {}".format(topic_word.shape))
+
+    # get the top 5 words for each topic (by probablity)
+    n = 5
+    for i, topic_dist in enumerate(topic_word):
+        topic_words = np.array(vocab)[np.argsort(topic_dist)][:-(n+1):-1]
+        print('*Topic {}\n- {}'.format(i, ' '.join(topic_words)))
+
+    # apply topic model to new test data set
+    doc_topic_test = model.transform(X_test)
+    for title, topics in zip(titles_test, doc_topic_test):
+        print("{} (top topic: {})".format(title, topics.argmax()))
+
+
+def enrichOSM(osmid,elementtype):
+    enriched = {}
+    osm = getOSMInfo(osmid, elementtype)
+    #print(osm)
+    enriched['osmtype']='|'.join(sorted(osm['keys']))
+    place = matchtoGoogleP(osm['name'],osm['lat'],osm['lon'])
+    #print(place.details)
+    enriched['website']=place.website
+    enriched['Gtype'] ='|'.join(sorted(place.types))
+    #print place.details['opening_hours']
+    #print place.details['reviews']
+    if 'reviews' in place.details.keys():
+        enriched['reviews']= '. '.join([r['text']+' ' for r in place.details['reviews']])
+    wt = placewebscraper.scrape(place.website)
+    enriched['webtext']= wt['text']
+    enriched['title'] = wt['title']
+    print(enriched)
+
+
+
+if __name__ == '__main__':
+##    osmid= 60018172
+##    osm = getOSMInfo(osmid)
+    osmid = 266614887
+    enrichOSM(osmid,'way')
+
+
--- placewebscraper.py
+++ placewebscraper.py
@@ -1,6 +1,6 @@
 #-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
+# Name:        placewebscraper
+# Purpose:      To fetch the informative title and text on a webpage describing a place
 #
 # Author:      simon
 #
@@ -8,65 +8,74 @@
 # Copyright:   (c) simon 2017
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
-from lxml import html
+
+
+#from lxml import html
 import requests
-import urllib2
+#import urllib2
 
 from bs4 import BeautifulSoup
 
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-
-
-url = "http://speeltuinbankaplein.nl/"
+#Test examples
+#url = "http://speeltuinbankaplein.nl/"
 #url = 'http://www.obsooginal.nl/johandewitt/'
-page = requests.get(url, headers=headers)
 
 
+def scrape(url):
+    resultobject = {}
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    page = requests.get(url, headers=headers)
+    #print page.content
+    #tree = html.fromstring(page.content)
 
-#print page.content
-tree = html.fromstring(page.content)
+    soup = BeautifulSoup(page.content)
 
-soup = BeautifulSoup(page.content)
+    resultobject['title'] = soup.title.string #soup.prettify()
 
-print soup.title.string#soup.prettify()
+    resultobject['text']=  clean(soup)
 
-##divs = soup.find_all("div")
-##ps = soup.find_all("p")
-##strongs =soup.find_all("strong")
-##bs=soup.find_all("b")
-##for d in divs:
-##    if d.string !=None:
-##      print d.bstring
-##
-##for p in ps:
-##    if p.string !=None:
-##        print p.string
-##
-##for b in bs:
-##    if b.string !=None:
-##        print b.string
-##
-##for strong in strongs:
-##    if strong.string !=None:
-##        print strong.string
+    ##divs = soup.find_all("div")
+    ##ps = soup.find_all("p")
+    ##strongs =soup.find_all("strong")
+    ##bs=soup.find_all("b")
+    ##for d in divs:
+    ##    if d.string !=None:
+    ##      print d.bstring
+    ##
+    ##for p in ps:
+    ##    if p.string !=None:
+    ##        print p.string
+    ##
+    ##for b in bs:
+    ##    if b.string !=None:
+    ##        print b.string
+    ##
+    ##for strong in strongs:
+    ##    if strong.string !=None:
+    ##        print strong.string
+
+    return resultobject
+
+def clean(soup):
+    # kill all script and style elements and all links
+    for script in soup(["script", "style", "a"]):
+        script.extract()    # rip it out
+
+    text = soup.get_text()
+
+    # break into lines and remove leading and trailing space on each
+    lines = (line for line in text.splitlines())
+    # break multi-headlines into a line each
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    # drop blank lines
+    text = '\n'.join(chunk for chunk in chunks if chunk)
+
+    #print(text)
+    return text
 
 
-# kill all script and style elements and all links
-for script in soup(["script", "style", "a"]):
-    script.extract()    # rip it out
-
-text = soup.get_text()
-
-# break into lines and remove leading and trailing space on each
-lines = (line for line in text.splitlines())
-# break multi-headlines into a line each
-chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-# drop blank lines
-text = '\n'.join(chunk for chunk in chunks if chunk)
-
-print(text)
-
-#print soup.get_text()
+if __name__ == '__main__':
+    print scrape('http://www.hoogcatharijne.nl/')
 
 
 ##buyers = tree.xpath('//div[@title="buyer-name"]/text()')
